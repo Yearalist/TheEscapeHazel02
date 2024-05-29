14556b8f5c543778aca8f123ae3398216420ef26
Begum Donmez <116170316+begumdonmez@users.noreply.github.com>
2024-05-29 18:09:13 +0300

    Menagers

---
+++ Assets/Scripts/Menagers/Inventory.cs
@@ -0,0 +1,167 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+public class Inventory : MonoBehaviour
+{
+    private CollectObject1 collectObjectScript;
+    private InspectObject inspectObjectScript;
+    private PickUpObject pickUpObjectScript;
+    
+    public List<GameObject> envanterSlotsList = new List<GameObject>(); // Envanter slotlarını tutan liste
+
+    private string collectingItemSpriteName;
+
+    private MeshRenderer meshRenderer;
+    
+    public Color activeColor = Color.yellow; // Aktif slotun rengi
+    public Color inactiveColor = Color.white; // Pasif slotların rengi
+
+    private int currentSlotIndex = -1; // Başlangıçta aktif slot yok
+    public int inventorySlotsNumber;
+    private int i = 0;
+    
+    private Transform envanterSlot;
+
+    void Start()
+    {
+        collectObjectScript= FindObjectOfType<CollectObject1>();
+        inspectObjectScript = FindObjectOfType<InspectObject>();
+        pickUpObjectScript = FindObjectOfType<PickUpObject>();
+        
+        inventorySlotsNumber = 3;
+        Debug.Log(inventorySlotsNumber);
+        
+        GameObject parentInvantoryObject=GameObject.FindWithTag("Inventory");
+        foreach (Transform child in parentInvantoryObject.transform)
+        {
+            envanterSlotsList.Add(child.gameObject);
+        }
+        
+        foreach (GameObject slot in envanterSlotsList)
+        {
+            Debug.Log("Child Name: " + slot.name);
+            slot.GetComponent<Image>().color = inactiveColor;
+        }
+    }
+
+    
+    void Update()
+    {
+        if (!inspectObjectScript.isInspecting)
+        {
+            NavigatingBetweenInventorySlots();
+        }
+       
+    }
+    
+    public void ShowSpriteCollectedItemOnInventory()
+    {
+        Debug.Log(envanterSlotsList[i].name);
+        Debug.Log(collectObjectScript.collectingItem.name);
+       
+       collectingItemSpriteName = collectObjectScript.collectingItem.name;
+       Sprite loadedSprite = Resources.Load<Sprite>(collectingItemSpriteName);
+       Image collectedItemImage = envanterSlotsList[i].GetComponent<Image>();
+       collectedItemImage.sprite = loadedSprite;
+       meshRenderer = collectObjectScript.collectingItem.GetComponent<MeshRenderer>();
+       collectObjectScript.collectingItem.GetComponent<Collider>().enabled = false;
+       meshRenderer.enabled = false;
+       
+       collectObjectScript.collectingItem = null;
+       inventorySlotsNumber--;
+       i++;
+    }
+    private void NavigatingBetweenInventorySlots()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scroll != 0)
+        {
+            
+            if (currentSlotIndex == -1)
+            {
+                currentSlotIndex = 0;
+                envanterSlotsList[currentSlotIndex].GetComponent<Image>().color = activeColor;
+                return;
+            }
+
+            
+            envanterSlotsList[currentSlotIndex].GetComponent<Image>().color = inactiveColor;
+
+            if (scroll > 0)
+            {
+                currentSlotIndex++;
+                if (currentSlotIndex >= envanterSlotsList.Count)
+                {
+                    currentSlotIndex = 0; // Başa dön
+                }
+                Debug.Log(envanterSlotsList[currentSlotIndex]);
+                ShowSpriteOnInventorySlot();
+            }
+            else if (scroll < 0)
+            {
+                currentSlotIndex--;
+                if (currentSlotIndex < 0)
+                {
+                    currentSlotIndex = envanterSlotsList.Count - 1; // Sona dön
+                }
+                ShowSpriteOnInventorySlot();
+            }
+
+           
+            envanterSlotsList[currentSlotIndex].GetComponent<Image>().color = activeColor;
+            
+          
+            
+        }
+    }
+
+    private void ShowSpriteOnInventorySlot()
+    {
+        Image onSlotItemImage = envanterSlotsList[currentSlotIndex].GetComponent<Image>();
+        string onSlotItemName = onSlotItemImage.sprite.name;
+        Debug.Log(onSlotItemName);
+
+        if (onSlotItemName!="Background"&&onSlotItemName!="SpriteY")
+        {
+            GameObject onSlotGameObject = GameObject.Find(onSlotItemName);
+           
+            if (onSlotGameObject != null)
+            {
+                if (pickUpObjectScript.isHolding)
+                {
+                    string holdingObjectName = pickUpObjectScript.holdingObject.name;
+                    Sprite holdingObectSprite = Resources.Load<Sprite>(holdingObjectName);
+                    onSlotItemImage.sprite = holdingObectSprite;
+                    meshRenderer = pickUpObjectScript.holdingObject.GetComponent<MeshRenderer>();
+                    meshRenderer.enabled = false;
+                    
+                    pickUpObjectScript.holdingObject = onSlotGameObject;
+                    meshRenderer = pickUpObjectScript.holdingObject.GetComponent<MeshRenderer>();
+                    meshRenderer.enabled = true;
+                    
+
+                }
+                else if (!pickUpObjectScript.isHolding)
+                {
+                    meshRenderer=onSlotGameObject.GetComponent<MeshRenderer>();
+                    meshRenderer.enabled = true;
+                    onSlotGameObject.transform.position = pickUpObjectScript.handPosition.transform.position;
+                    pickUpObjectScript.holdingObject = onSlotGameObject;
+                    pickUpObjectScript.isHolding = true;
+                    
+
+                }
+            }
+            else
+            {
+                Debug.Log("null");
+            }
+        }
+        
+        
+    }
+    
+    
+}
---
+++ Assets/Scripts/Menagers/PickUpObject.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Sprites;
+using UnityEngine.UI;
+using UnityEngine;
+public class PickUpObject : MonoBehaviour
+{
+    private InspectObject inspectObjectScript;
+    private Inventory inventoryScript;
+
+    public Transform handPosition;//Elin olması gereken pozisyon
+    
+    private Rigidbody holdingObjectRigidbody;//Eldeki objenin Rigidbodysi
+    
+    public GameObject holdingObject;//Eldeki obje
+    
+    private Sprite spriteY;
+
+    private string spriteYName = "SpriteY";
+    
+    public bool isHolding = false;//Elimde obje var mı
+
+    RaycastHit hit;
+    
+    void Start()
+    {
+        inspectObjectScript = FindObjectOfType<InspectObject>();
+        inventoryScript = FindObjectOfType<Inventory>();
+        string scriptName = this.GetType().Name;
+        Debug.Log("Hello There is " + scriptName);
+        Debug.Log("Holding Control "+ isHolding);
+        spriteY=Resources.Load<Sprite>(spriteYName);
+    }
+
+    
+    void Update()
+    {
+        if (!isHolding&&!inspectObjectScript.isInspecting)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.TryGetComponent(out IInteractable interactObject))
+                    { 
+                        interactObject.InteractableObjects();
+                       holdingObject = hit.collider.gameObject;
+                       PickUpItem();
+                       
+                    }
+                }
+            }
+        }
+
+        else if (isHolding&&!inspectObjectScript.isInspecting)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                DropItem();
+                
+            }
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (isHolding&&!inspectObjectScript.isInspecting)
+        {
+            var holdingObjectCollider = holdingObject.GetComponent<Collider>();
+            if (holdingObjectCollider.enabled == false)
+            {
+                holdingObjectCollider.enabled = true;
+            }
+            holdingObject.transform.position = handPosition.transform.position;
+            var rigidBody = holdingObject.GetComponent<Rigidbody>();
+            var moveTo=handPosition.transform.position;
+            var differance = moveTo - holdingObject.transform.position;
+            rigidBody.AddForce(differance*500);
+            holdingObject.transform.rotation = handPosition.rotation;
+        }
+        
+    }
+
+    void PickUpItem()
+    {
+        Debug.Log("Aldin");
+        Debug.Log(holdingObject.name);
+        isHolding = true;
+        holdingObject.transform.position =
+            Vector3.Lerp(holdingObject.transform.position, handPosition.transform.position, 0.4f);
+        holdingObjectRigidbody = holdingObject.GetComponent<Rigidbody>();
+        holdingObjectRigidbody.constraints = RigidbodyConstraints.FreezeRotation;//Rotasyonunu dondur
+        holdingObjectRigidbody.drag = 25f;
+        holdingObjectRigidbody.useGravity = false;
+        
+
+    }
+
+    void DropItem()
+    {
+        foreach (GameObject slots in inventoryScript.envanterSlotsList)
+        {
+            Debug.Log("Child Name 2: " + slots.name);
+            //string slotsImage = slots.GetComponent<Image>().sprite.name;
+            if (holdingObject.name == slots.GetComponent<Image>().sprite.name)
+            {
+                Debug.Log("Buraya geldi");
+                slots.GetComponent<Image>().sprite = spriteY;
+            }
+
+        }
+        var rigidBody = holdingObject.GetComponent<Rigidbody>();
+        rigidBody.drag = 1f;
+        rigidBody.useGravity = true;
+        rigidBody.constraints = RigidbodyConstraints.None;
+        holdingObject = null;
+        isHolding = false;
+        Debug.Log("biraktin");
+    }
+    
+}
